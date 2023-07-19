60efd249a4db920d07f3a7487f1356c18c341cfa
kumail imran <kumail2306@gmail.com>
2023-07-19 16:07:41 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import './reset.css';
 import './App.css';
 
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-import Main from './components/Main/Main';
+import { useState } from "react";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Main from "./components/Main/Main";
+import SelectedBeast from "./components/SelectedBeast/SelectedBeast";
 
+import data from "./data.json";
 
 
 function App() {
+  const [modal, setModal] = useState(false);
+  const [modalContent, setModalContent] = useState({});
+
+
+  function handleModal(beast) {
+    setModal(!modal);
+    setModalContent(beast);
+  }
+
+  function closeModal() {
+    setModal(!modal);
+    setModalContent({});
+  }
+
   return (
     <div className="App">
       <Header />
-      <Main />
+      <Main data={data} handleModal={handleModal} />
+      {modal && <SelectedBeast modalContent={modalContent} closeModal={closeModal} />}
       <Footer />
     </div>
   );
--- src/components/HornedBeast/HornedBeast.js
+++ src/components/HornedBeast/HornedBeast.js
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import "./HornedBeast.css";
 
-export default function HornedBeast({ title, imgUrl, description }) {
+export default function HornedBeast({ title, imgUrl, description, handleModal, beastObject }) {
   const [fav, setfav] = useState(0);
 
   function handleFav() {
@@ -11,7 +11,7 @@ export default function HornedBeast({ title, imgUrl, description }) {
   return (
     <div className="container">
       <h2>{title}</h2>
-      <img src={imgUrl} alt={title} />
+      <img src={imgUrl} alt={title} onClick={() => handleModal(beastObject)} />
       <p>{description}</p>
       <p>
       <span className="heart" onClick={handleFav}>
